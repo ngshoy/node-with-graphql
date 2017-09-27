1bad9c207c02bfa6645fb43e26c209c30002a7de
Jack Zhang <jack.zhang@openlane.com>
2017-09-27 10:15:20 -0400

    added subscription

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const {createServer} = require('http');
 const express = require('express');
 const bodyParser = require('body-parser');
 const {graphqlExpress, graphiqlExpress} = require('graphql-server-express');
+const {SubscriptionServer} = require('subscriptions-transport-ws');
+const {subscribe, execute} = require('graphql');
 const schema = require('./schema');
 
 const app = express();
@@ -16,13 +18,24 @@ app.use('/graphql', graphqlExpress({
 }));
 
 app.use('/graphiql', graphiqlExpress({
-    endpointURL: '/graphql'
+    endpointURL: '/graphql',
+    subscriptionsEndpoint: `ws://localhost:${PORT}/subscriptions`
 }));
 
 const server = createServer(app);
 
 server.listen(PORT, err => {
     if (err) throw err;
+
+    new SubscriptionServer({
+        schema,
+        execute,
+        subscribe,
+        onConnect: () => console.log('Client connected')
+    }, {
+        server,
+        path: '/subscriptions'
+    });
 
     console.log(`> Ready on PORT ${PORT}`);
 });
