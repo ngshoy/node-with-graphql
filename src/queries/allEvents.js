0cd1b9a3bf0a7940fe5366dc37b2be4de10a725a
Jack Zhang <jack.zhang@openlane.com>
2017-09-27 11:15:11 -0400

    added pagination

--- src/queries/allEvents.js
+++ src/queries/allEvents.js
@@ -1,14 +1,26 @@
-const {GraphQLList} = require('graphql');
+const {GraphQLList, GraphQLInt} = require('graphql');
 const EventType = require('../types/event');
 const getProjection = require('../utils/projection');
 
 module.exports = {
     type: new GraphQLList(EventType),
-    resolve: (root, args, {db: {Event}}, fieldASTs) => {
+    args: {
+        first: {
+            name: "first",
+            type: GraphQLInt
+        },
+        skip: {
+            name: "skip",
+            type: GraphQLInt
+        }
+    },
+    resolve: (root, {first = 16, skip = 0}, {db: {Event}}, fieldASTs) => {
         return new Promise((resolve, reject) => {
             const projection = getProjection(fieldASTs);
             Event.find({})
                 .select(projection)
+                .skip(skip)
+                .limit(first)
                 .exec()
                 .then(data => resolve(data))
                 .catch(errors => reject(errors))
